e6fb5c91168f402ad894c2858dea06d4be2b002f
robertsaternus <robert.saternus@landingi.com>
2019-11-17 08:52:20 +0100

    Install annotations & create first controller

---
+++ backend/src/Controller/PageController.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class PageController extends AbstractController
+{
+    /** @Route("/") */
+    public function index() : Response
+    {
+        return new Response('Elo man!');
+    }
+
+}
